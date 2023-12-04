3d41c81fbf406e63265ea72cfc7d641afce3ad44
unknown <enzovizotto57@gmail.com>
2023-12-04 16:54:32 -0300

    TIN-14 remodelando tela

--- frontend/src/pages/ListJobs/style.js
+++ frontend/src/pages/ListJobs/style.js
@@ -4,12 +4,35 @@ export const ContainerListJobs = styled.div`
 
     .container{
         padding: 1rem;
+        border-bottom: solid 0.25rem;
+        border-color: var(--cyan);
     }
 
     .box{
         display: flex;
         justify-content: space-between;
+        flex-wrap: wrap;
     }
 
+    .title {
+        padding-bottom: 1rem;
+    }
+
+    .mainbox {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    .box figure {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+    }
+
+
+`
+
+export const figure = styled.div`
 
 `
